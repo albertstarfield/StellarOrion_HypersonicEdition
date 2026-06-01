24c283731134206474233623b92725bf2862a424
Albert Starfield Wahyu Suryo Sam <albertstarfield2001@gmail.com>
2026-06-01 12:32:28 +0700

    fix(docs): format LBMZero2Hero markdown and compile A4 pdf

---
+++ resume_opt.py
@@ -0,0 +1,21 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
+from StellarOrionEngineMach5Up import Api
+app = Api()
+
+opt_params = {
+    'target_vehicle': 'IRVE-3',
+    'opt_method': 'random',
+    'opt_samples': '2',
+    'env_mach': '5.0',
+    'env_alt': '40.0',
+    'env_run': '500',
+    'env_fnum': '2.5e20',
+    'sparta_gpu': False,
+    'default_payload': False,
+    'payload_file': 'CADDesign/HIAD_custom_full.step',
+    'active_params': {'diameter': True, 'angle': True, 'nose': True, 'toroids': True, 'thickness': True, 'scallop_pts': True, 'scallop_angle': True}
+}
+
+app.execute_optimization(opt_params)
---
+++ run_optimization.py
@@ -0,0 +1,22 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
+from StellarOrionEngineMach5Up import Api
+app = Api()
+
+opt_params = {
+    'target_vehicle': 'IRVE-3',
+    'opt_method': 'random',
+    'samples': 1,
+    'env_mach': '17.6',
+    'env_alt': '40.0',
+    'env_run': '2500',
+    'env_fnum': '2.5e20',
+    'env_cores': 10,
+    'sparta_gpu': False,
+    'default_payload': False,
+    'payload_file': 'CADDesign/HIAD_custom_full.step',
+    'active_params': {'diameter': True, 'angle': True, 'nose': True, 'toroids': True, 'thickness': True, 'scallop_pts': True, 'scallop_angle': True}
+}
+
+app.execute_optimization(opt_params)
